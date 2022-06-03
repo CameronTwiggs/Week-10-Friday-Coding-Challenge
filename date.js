ce58d5aa59a5ed6d9afb25ec6fecdd8d9fa4de20
CameronTwiggs <cameron.020617@gmail.com>
2022-06-03 14:08:32 -0400

    adds comments

--- date.js
+++ date.js
@@ -1,9 +1,9 @@
 function dayCount (date1, date2) {
-    const date1MS = (new Date(date1)).getTime();
-    const date2MS = (new Date(date2)).getTime();
-    const time = date1MS - date2MS;
+    const date1MS = (new Date(date1)).getTime(); // date1 in milliseconds
+    const date2MS = (new Date(date2)).getTime(); // date2 in milliseconds
+    const time = date1MS - date2MS; 
     const convert = Math.abs(((((time / 1000) / 60) / 60) / 24));
     return `Its been ${convert} days! that makes about ${Math.floor(convert / 365)} years! `;
 }
 
-console.log(dayCount("May 15, 2015", "06/22/21"))
+console.log(dayCount("May 15, 2015", "06/3/22"))
