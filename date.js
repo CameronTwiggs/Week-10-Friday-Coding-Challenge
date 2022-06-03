a08d4be6b086d0cec965e02f7f9829c16512b874
CameronTwiggs <cameron.020617@gmail.com>
2022-06-03 13:57:03 -0400

    cleans up code

--- date.js
+++ date.js
@@ -1,6 +1,3 @@
-let date = new Date("May 3, 2022")
-let tommarow = new Date("June 22, 2022")
-
 function dayCount (date1, date2) {
     const date1MS = (new Date(date1)).getTime();
     const date2MS = (new Date(date2)).getTime();
